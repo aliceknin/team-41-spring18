39bf33f7731083b98ccb8a8b86b587c19ec1eec7
nicolepristin <pristin.nicole@husky.neu.edu>
2018-03-12 21:00:37 -0400

    Change snake case fields to camel case

--- project/spoiled-tomatillos-team41/src/main/java/edu/neu/cs4500/controllers/friend/Friend.java
+++ project/spoiled-tomatillos-team41/src/main/java/edu/neu/cs4500/controllers/friend/Friend.java
@@ -10,27 +10,27 @@ public class Friend {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private int user_id;
-    private int friend_id;
+    private int userId;
+    private int friendId;
 
-    public Friend(int user_id, int friend_id) {
-        this.user_id = user_id;
-        this.friend_id = friend_id;
+    public Friend(int userId, int friendId) {
+        this.userId = userId;
+        this.friendId = friendId;
     }
 
-    public int getUser_id() {
-        return user_id;
+    public int getUserId() {
+        return userId;
     }
 
-    public void setUser_id(int user_id) {
-        this.user_id = user_id;
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 
-    public int getFriend_id() {
-        return friend_id;
+    public int getFriendId() {
+        return friendId;
     }
 
-    public void setFriend_id(int friend_id) {
-        this.friend_id = friend_id;
+    public void setFriendId(int friendId) {
+        this.friendId = friendId;
     }
 }
