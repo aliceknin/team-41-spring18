88e71e83b7c168d4b1f3bca49fd0a5c9e33fa800
nicolepristin <pristin.nicole@husky.neu.edu>
2018-03-25 12:25:34 -0400

    Encrype user passwords using SHA-256

--- project/spoiled-tomatillos-team41/src/main/java/edu/neu/cs4500/controllers/user/UserController.java
+++ project/spoiled-tomatillos-team41/src/main/java/edu/neu/cs4500/controllers/user/UserController.java
@@ -1,19 +1,18 @@
 package edu.neu.cs4500.controllers.user;
 
-import java.net.URI;
 import java.net.URISyntaxException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.WebServiceException;
 
 @RestController
 public class UserController {
@@ -26,14 +25,18 @@ public class UserController {
 	@RequestMapping("/api/user/create/{username}/{pw}/{email}/{fullName}")
 	public ResponseEntity<User> createEndUser(@PathVariable("username") String username, @PathVariable("pw") String pw,
 						   @PathVariable("email") String email, @PathVariable("fullName") String fullName)
-							throws URISyntaxException {
+			throws URISyntaxException, NoSuchAlgorithmException {
 
 		if (userRepository.findByUsername(username).size() != 0) {
 			throw new IllegalArgumentException("A user with this username already exists");
 		} else if (userRepository.findByEmail(email).size() != 0) {
 			throw new IllegalArgumentException("A user with this email already exists");
 		} else {
-			User user = new User(username, pw, email, fullName, false);
+			MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+			messageDigest.update(pw.getBytes());
+			String encryptedString = new String(messageDigest.digest());
+
+			User user = new User(username, encryptedString, email, fullName, false);
 			userRepository.save(user);
 			return new ResponseEntity<>(user, HttpStatus.OK);
 
@@ -43,14 +46,14 @@ public class UserController {
 	// Returns all the users registered with the system
 	@RequestMapping("/api/user/select/all")
 	public List<User> selectAllUserObjects() {
-		List<User> users = (List<User>) userRepository.findAll();
+		List<User> users = userRepository.findAll();
 		return users;
 	}
 
 	// Returns all usernames that contain the given query
 	@RequestMapping("/api/user/select/{query}")
 	public List<String> selectUsernamesContainingQuery(@PathVariable("query") String query) {
-		List<User> users = (List<User>) userRepository.findByUsernameContaining(query);
+		List<User> users = userRepository.findByUsernameContaining(query);
 		List<String> usernames = new ArrayList<>();
 		for (User user : users) {
 			usernames.add(user.username);
@@ -60,8 +63,12 @@ public class UserController {
 
 	// Verifies that the user can login by checking if the provided username and password match a record
 	@RequestMapping("/api/user/verify/{username}/{password}")
-	public boolean verifyLogin(@PathVariable("username") String username, @PathVariable("password") String pw) {
-		 List<User> users = userRepository.findByUsernameAndPassword(username, pw);
+	public boolean verifyLogin(@PathVariable("username") String username, @PathVariable("password") String pw) throws NoSuchAlgorithmException {
+		MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+		messageDigest.update(pw.getBytes());
+		String encryptedString = new String(messageDigest.digest());
+
+		List<User> users = userRepository.findByUsernameAndPassword(username, encryptedString);
 		 return users.size() != 0;
 	}
 
