f08227f5ae151cbeb642ef4ca2bd15ad304d48ff
nicolepristin <pristin.nicole@husky.neu.edu>
2018-03-24 21:53:21 -0400

    Achieve 100% code, branch, and line coverage

--- project/spoiled-tomatillos-team41/src/main/java/edu/neu/cs4500/controllers/user/UserController.java
+++ project/spoiled-tomatillos-team41/src/main/java/edu/neu/cs4500/controllers/user/UserController.java
@@ -1,19 +1,15 @@
 package edu.neu.cs4500.controllers.user;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.xml.ws.WebServiceException;
 
 @RestController
 public class UserController {
