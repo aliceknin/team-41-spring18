9939eb17987b6118b9e251d91d8bae947bbe0cb3
nicolepristin <pristin.nicole@husky.neu.edu>
2018-03-24 23:58:39 -0400

    Add all tests for 100% code coverage

--- project/spoiled-tomatillos-team41/src/test/java/edu/neu/cs4500/RecommendationTest.java
+++ project/spoiled-tomatillos-team41/src/test/java/edu/neu/cs4500/RecommendationTest.java
@@ -7,7 +7,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RecommendationTest {
@@ -16,5 +23,83 @@ public class RecommendationTest {
     private RecommendationController recController;
     @Mock
     private RecommendationRepository recRepository;
-   
+
+    @Test
+    public void addRecommendationTest() {
+        Recommendation rec = new Recommendation(17, 41, "sdg8275");
+
+        Mockito.when(recRepository.save(rec)).thenReturn(rec);
+        Recommendation rec2 =
+                recController.addRecommendation(17, 41, "sdg8275");
+        assertEquals(rec2.getRecFromUserId(), 17);
+        assertEquals(rec2.getRecToUserId(), 41);
+        assertEquals(rec2.getIMDBMovieId(), "sdg8275");
+    }
+
+    @Test
+    public void getUserRecommendationsTest() {
+        Recommendation rec1 = new Recommendation(947, 163, "poiw284");
+        Recommendation rec2 = new Recommendation(3728, 163, "ksdgj98");
+        Recommendation rec3 = new Recommendation(17, 163, "BMNsd2873");
+
+        List<Recommendation> recs = new ArrayList<>();
+        recs.add(rec1);
+        recs.add(rec2);
+        recs.add(rec3);
+
+        Mockito.when(recRepository.findByRecToUserId(163)).thenReturn(recs);
+        List<Recommendation> recsForUser = recController.getUserRecommendations(163);
+
+        assertEquals(recsForUser.size(),  3);
+        assertTrue(recsForUser.contains(rec1));
+        assertTrue(recsForUser.contains(rec2));
+        assertTrue(recsForUser.contains(rec3));
+    }
+
+    /*
+    @RequestMapping("/api/recommendation/{to_user_id}/select/movies")
+    public List<String> getUserRecommendedMovies(@PathVariable("to_user_id") int userId) {
+        List<String> movies = new ArrayList<>();
+
+        List<Recommendation> recommendations = recRepository.findByRecToUserId(userId);
+        for (Recommendation rec : recommendations) {
+            String movieId = rec.getIMDBMovieId();
+            movies.add(movieId);
+        }
+        return movies;
+    }
+     */
+    @Test
+    public void getUserRecommendedMoviesTest() {
+        Recommendation rec1 = new Recommendation(947, 163, "poiw284");
+        Recommendation rec2 = new Recommendation(3728, 163, "ksdgj98");
+        Recommendation rec3 = new Recommendation(17, 163, "BMNsd2873");
+
+        List<Recommendation> recs = new ArrayList<>();
+        recs.add(rec1);
+        recs.add(rec2);
+        recs.add(rec3);
+
+        Mockito.when(recRepository.findByRecToUserId(163)).thenReturn(recs);
+        List<String> recommendedMovies  = recController.getUserRecommendedMovies(163);
+
+        assertEquals(recommendedMovies.size(),  3);
+        assertTrue(recommendedMovies.contains("poiw284"));
+        assertTrue(recommendedMovies.contains("ksdgj98"));
+        assertTrue(recommendedMovies.contains("BMNsd2873"));
+    }
+
+    @Test
+    public void recommendationGettersAndSettersTest() {
+        Recommendation rec = new Recommendation();
+        rec.setId(83);
+        rec.setRecFromUserId(91);
+        rec.setRecToUserId(872);
+        rec.setIMDBMovieId("sdkgj927");
+
+        assertEquals(rec.getId(), 83);
+        assertEquals(rec.getRecFromUserId(), 91);
+        assertEquals(rec.getRecToUserId(), 872);
+        assertEquals(rec.getIMDBMovieId(), "sdkgj927");
+    }
 }
